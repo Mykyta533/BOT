535248717ff7d66894017efa5b8e7417678aa227
Mykyta533 <mykyta.synelnykov@gmail.com>
2026-09-09 08:20:18 +0300

    Updated config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || 'https://ocnwpbzrmpiniocmasny.supabase.co';
 export const SUPABASE_ANON_KEY = (import.meta.env.VITE_SUPABASE_ANON_KEY as string) ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jbndwYnpybXBpbmlvY21hc255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3MjE2MzksImV4cCI6MjEwMTI5NzYzOX0.3XVU2sAbXCK2-EHdjcQgd-s1PtCGYo10-ocJHKiWbYc';
+  'sb_publishable_eF7pxeFUMcyBT2I6_dNGcQ_a-DxuBbu';
 export const FN_URL = `${SUPABASE_URL}/functions/v1/telegram-bot`;
